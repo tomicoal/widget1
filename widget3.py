22e83b644efcb3940ad5e30cae34e62922a26983
Tomas <tmico@gmail.com>
2024-08-07 15:03:13 +0100

    widget 3 intial customazion examples done

--- widget3.py
+++ widget3.py
@@ -1,4 +1,17 @@
 import customtkinter as ctk
+from random import  choice
+
+
+# functions
+def move_btn():
+    global button_x
+    button_x += 0.05
+    button.place(relx=button_x, rely=0.5, relheight=button_x, anchor="center")
+
+    # configure
+    colors = ["red", "navy", "orange", "yellow"]
+    button.configure(fg_color=choice(colors))
+
 
 # window
 window = ctk.CTk()
@@ -7,8 +20,8 @@ window.geometry("600x400")
 
 # button
 button_x = 0.5
-button = ctk.CTkButton(window,text="toggle sidebar")
-button.place(relx=button_x, rely=0.5, anchor="center")
+button = ctk.CTkButton(window, text="toggle sidebar", command=move_btn, fg_color="blue")
+button.place(relx=button_x, rely=0.5, relheight=button_x, anchor="center")
 
 # run
 window.mainloop()
